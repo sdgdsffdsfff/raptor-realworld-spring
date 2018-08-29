c14929b1d839214d9441511dd1b045d4c5c309ca
张呈熹 <zhangchengxi@ppdai.com>
2018-08-29 22:14:57 +0800

    添加proto文件之后编译通过

--- src/main/java/io/spring/api/security/CORSConfig.java
+++ src/main/java/io/spring/api/security/CORSConfig.java
@@ -3,9 +3,10 @@ package io.spring.api.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CORSConfig implements WebMvcConfigurer {
+public class CORSConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")

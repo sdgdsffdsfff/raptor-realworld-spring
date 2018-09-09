cf9a80f18c72b15eb0c0b6eac4cfd60677b726d2
lizhiming <lizhiming@ppdai.com>
2018-09-09 15:58:25 +0800

    merge

--- src/main/java/io/spring/core/user/User.java
+++ src/main/java/io/spring/core/user/User.java
@@ -4,7 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.commons.lang3.StringUtils;
+
 
 import java.util.UUID;
 
